c0817d49e0f89cff19642032e7b6182395a9504e
ganve <ganve@192.168.0.132>
2020-02-04 16:34:09 +0530

    commit and push

--- MyfirstApp/src/info/inetsolv/HelloWorld.java
+++ MyfirstApp/src/info/inetsolv/HelloWorld.java
@@ -3,8 +3,8 @@ package info.inetsolv;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world ");
-	
+	System.out.println("Hello world");
+
 	}
 
 }
